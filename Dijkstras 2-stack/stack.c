8f8dc965cb01b02279c5a27c88fb700b9c362e29
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-03-13 07:54:42 -0300

    Created the Stack files

---
+++ Dijkstras 2-stack/stack.c
@@ -0,0 +1,21 @@
+//
+// Created by Pichau on 12/03/2020.
+//
+
+#include "stack.h"
+
+struct numberNode {
+    int operator;
+    Node* before;
+};
+
+struct operationNode {
+    int operator;
+    Node* before;
+};
+
+// TODO create a Node with the content of the string the user write
+
+// TODO stack the nodes to form a
+
+
---
+++ Dijkstras 2-stack/stack.h
@@ -0,0 +1,10 @@
+//
+// Created by Pichau on 12/03/2020.
+//
+
+#ifndef DIJKSTRAS_2_STACK_STACK_H
+#define DIJKSTRAS_2_STACK_STACK_H
+
+typedef struct numberNode Node;
+
+#endif //DIJKSTRAS_2_STACK_STACK_H
